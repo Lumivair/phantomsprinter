165a3e639e0f76e309b3ed5f2a48d2f3d9c51f33
Lumivair <lumivair@gmail.com>
2026-04-09 10:05:24 +0200

    start game + F3 menu + coordinates

--- main.py
+++ main.py
@@ -1,1 +1,54 @@
 # main PhantomSprinter game
+
+# imports
+import pygame
+
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+# asset setup
+font = pygame.font.Font("assets/font/Saira_Stencil/static/SairaStencil-SemiBold.ttf", 50)
+
+
+# reset variables
+debug = False
+player_x = 0
+player_y = 0
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_F3:
+                if debug == True:
+                    debug = False
+                elif debug == False:
+                    debug = True
+
+
+    key=pygame.key.get_pressed()
+    if key[pygame.K_RIGHT]:
+        player_x += 1
+    if key[pygame.K_LEFT]:
+        player_x -= 1
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
+    debug_menu = font.render(f"X: {player_x}    Y: {player_y}", True, "red")
+    # RENDER YOUR GAME HERE
+    
+    if debug == True:
+        screen.blit(debug_menu, (0,0))
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
